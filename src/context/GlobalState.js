8a1d80e9816d79b2d79f2d9281eb35b7ff65d0fe
Cezara Luca <cezaraluca@yahoo.com>
2021-02-06 19:13:42 +0200

    romanian initial state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,10 +4,10 @@ import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
     transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
+          { id: 1, text: 'Carti', amount: -20 },
+          { id: 2, text: 'Salariu', amount: 800 },
+          { id: 3, text: 'Bicicleta', amount: -300 },
+          { id: 4, text: 'Alocatie', amount: 150 }
         ]
 }
 
